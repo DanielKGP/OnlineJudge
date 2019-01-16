51a850bf220756f3107385e606cd7227396888eb
DanielKGP <ganpengkang@gmail.com>
2019-01-16 11:53:46 -0800

    add Problem

--- oj-server/server.js
+++ oj-server/server.js
@@ -1,10 +1,13 @@
 var express = require("express");
-
 var app = express()
+var restRouter = require("./routes/rest");
+
+app.use("/api/v1", restRouter);
 
 app.get('/', function (req, res){
     res.send('Hello Express World!')
 })
+
 
 app.listen(3000, function () {
     console.log('App listening on port 3000!')
